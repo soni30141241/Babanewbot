62ec53c6812dcdbc1f4db7f0ff67b7797cea431c
soni30141241 <soni30141241@gmail.com>
2026-02-11 16:04:39 +0530

    Fix comment formatting in config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/soni30141241/Babanewbot",
 
-    # Vars For API End Pont.
+# Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
 YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
